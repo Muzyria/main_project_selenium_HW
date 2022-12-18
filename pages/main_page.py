0812028bf040de85e1c6b1a8c1acf64ca74fb83e
Muzyria <fila090580@gmail.com>
2022-12-18 20:39:16 +0200

    new

--- pages/main_page.py
+++ pages/main_page.py
@@ -14,31 +14,31 @@ class Main_page(Base):
     # Locators
 
     select_product_1 = '//button[@id="add-to-cart-sauce-labs-backpack"]'
-    select_product_2 = '//button[@id="add-to-cart-sauce-labs-bike-light"]'
-    select_product_3 = '//button[@id="add-to-cart-sauce-labs-bolt-t-shirt"]'
-    cart = '//a[@class="shopping_cart_link"]'
-    menu = '//button[@id="react-burger-menu-btn"]'
-    link_about = '//a[@id="about_sidebar_link"]'
+    # select_product_2 = '//button[@id="add-to-cart-sauce-labs-bike-light"]'
+    # select_product_3 = '//button[@id="add-to-cart-sauce-labs-bolt-t-shirt"]'
+    # cart = '//a[@class="shopping_cart_link"]'
+    # menu = '//button[@id="react-burger-menu-btn"]'
+    # link_about = '//a[@id="about_sidebar_link"]'
 
     # Getters
 
     def get_select_product_1(self):
         return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.select_product_1)))
 
-    def get_select_product_2(self):
-        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.select_product_2)))
-
-    def get_select_product_3(self):
-        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.select_product_3)))
-
-    def get_cart(self):
-        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.cart)))
-
-    def get_menu(self):
-        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.menu)))
-
-    def get_lint_about(self):
-        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.link_about)))
+    # def get_select_product_2(self):
+    #     return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.select_product_2)))
+    #
+    # def get_select_product_3(self):
+    #     return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.select_product_3)))
+    #
+    # def get_cart(self):
+    #     return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.cart)))
+    #
+    # def get_menu(self):
+    #     return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.menu)))
+    #
+    # def get_lint_about(self):
+    #     return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.link_about)))
 
     # Actions
 
@@ -46,26 +46,26 @@ class Main_page(Base):
         self.get_select_product_1().click()
         print('Click select product_1')
 
-    def click_select_product_2(self):
-        self.get_select_product_2().click()
-        print('Click select product_2')
-
-    def click_select_product_3(self):
-        self.get_select_product_3().click()
-        print('Click select product_3')
-
-
-    def click_cart(self):
-        self.get_cart().click()
-        print('Click cart')
-
-    def click_menu(self):
-        self.get_menu().click()
-        print('Click menu')
-
-    def click_link_about(self):
-        self.get_lint_about().click()
-        print('Click link about')
+    # def click_select_product_2(self):
+    #     self.get_select_product_2().click()
+    #     print('Click select product_2')
+    #
+    # def click_select_product_3(self):
+    #     self.get_select_product_3().click()
+    #     print('Click select product_3')
+    #
+    #
+    # def click_cart(self):
+    #     self.get_cart().click()
+    #     print('Click cart')
+    #
+    # def click_menu(self):
+    #     self.get_menu().click()
+    #     print('Click menu')
+    #
+    # def click_link_about(self):
+    #     self.get_lint_about().click()
+    #     print('Click link about')
 
     # Methods
 
@@ -74,18 +74,18 @@ class Main_page(Base):
         self.click_select_product_1()
         self.click_cart()
 
-    def select_products_2(self):
-        self.get_current_url()
-        self.click_select_product_2()
-        self.click_cart()
-
-    def select_products_3(self):
-        self.get_current_url()
-        self.click_select_product_3()
-        self.click_cart()
-
-    def select_menu_about(self):
-        self.get_current_url()
-        self.click_menu()
-        self.click_link_about()
-        self.assert_url('https://saucelabs.com/')
+    # def select_products_2(self):
+    #     self.get_current_url()
+    #     self.click_select_product_2()
+    #     self.click_cart()
+    #
+    # def select_products_3(self):
+    #     self.get_current_url()
+    #     self.click_select_product_3()
+    #     self.click_cart()
+    #
+    # def select_menu_about(self):
+    #     self.get_current_url()
+    #     self.click_menu()
+    #     self.click_link_about()
+    #     self.assert_url('https://saucelabs.com/')
--- tests/test_by_product.py
+++ tests/test_by_product.py
@@ -8,12 +8,12 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 
-from pages.cart_page import Cart_page
-from pages.client_information_page import Client_information_page
-from pages.finish_page import Finish_page
-from pages.login_page import Login_page
-from pages.main_page import Main_page
-from pages.payment_page import Payment_page
+# from pages.cart_page import Cart_page
+# from pages.client_information_page import Client_information_page
+# from pages.finish_page import Finish_page
+# from pages.login_page import Login_page
+# from pages.main_page import Main_page
+# from pages.payment_page import Payment_page
 
 
 # @pytest.mark.run(order=1)
@@ -25,58 +25,58 @@ def test_by_product_1(set_up, set_group):
     login = Login_page(driver)
     login.authorization()
 
-    mp = Main_page(driver)
-    mp.select_products_1()
-
-    cp = Cart_page(driver)
-    cp.product_confirmation()
-
-    cip = Client_information_page(driver)
-    cip.input_information()
-
-    pp = Payment_page(driver)
-    pp.payment()
-
-    f = Finish_page(driver)
-    f.finish()
-
-    # time.sleep(5)
-    driver.quit()
-
-
-# @pytest.mark.run(order=2)
-def test_by_product_2(set_up, set_group):
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-
-    print('Start tests 2')
-
-    login = Login_page(driver)
-    login.authorization()
-
-    mp = Main_page(driver)
-    mp.select_products_2()
-
-    cp = Cart_page(driver)
-    cp.product_confirmation()
+    # mp = Main_page(driver)
+    # mp.select_products_1()
+    #
+    # cp = Cart_page(driver)
+    # cp.product_confirmation()
+    #
+    # cip = Client_information_page(driver)
+    # cip.input_information()
+    #
+    # pp = Payment_page(driver)
+    # pp.payment()
+    #
+    # f = Finish_page(driver)
+    # f.finish()
 
     # time.sleep(5)
     driver.quit()
 
 
-# @pytest.mark.run(order=3)
-def test_by_product_3(set_up, set_group):
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+# # @pytest.mark.run(order=2)
+# def test_by_product_2(set_up, set_group):
+#     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+#
+#     print('Start tests 2')
+#
+#     login = Login_page(driver)
+#     login.authorization()
+#
+#     mp = Main_page(driver)
+#     mp.select_products_2()
+#
+#     cp = Cart_page(driver)
+#     cp.product_confirmation()
+#
+#     # time.sleep(5)
+#     driver.quit()
 
-    print('Start tests 3')
 
-    login = Login_page(driver)
-    login.authorization()
-
-    mp = Main_page(driver)
-    mp.select_products_3()
-
-    cp = Cart_page(driver)
-    cp.product_confirmation()
-
-    # time.sleep(5)
-    driver.quit()
+# # @pytest.mark.run(order=3)
+# def test_by_product_3(set_up, set_group):
+#     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+#
+#     print('Start tests 3')
+#
+#     login = Login_page(driver)
+#     login.authorization()
+#
+#     mp = Main_page(driver)
+#     mp.select_products_3()
+#
+#     cp = Cart_page(driver)
+#     cp.product_confirmation()
+#
+#     # time.sleep(5)
+#     driver.quit()
