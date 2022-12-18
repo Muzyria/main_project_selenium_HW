3beb9a8f89168717bb03702e712b14a1050c802a
Muzyria <fila090580@gmail.com>
2022-12-18 20:59:37 +0200

    new

--- tests/test_by_product.py
+++ tests/test_by_product.py
@@ -17,26 +17,8 @@ def test_by_product_1():
 
     print('Start tests 1')
 
-    # login = Login_page(driver)
-    # login.authorization()
-
     mp = Main_page(driver)
     mp.open_main_page()
-    #
-    # cp = Cart_page(driver)
-    # cp.product_confirmation()
-    #
-    # cip = Client_information_page(driver)
-    # cip.input_information()
-    #
-    # pp = Payment_page(driver)
-    # pp.payment()
-    #
-    # f = Finish_page(driver)
-    # f.finish()
 
     time.sleep(5)
     driver.quit()
-
-
-
