72420fb26ed4ccdfcfb8ca145824d4fe8ee5469e
“Muzyria” <fila090580@gmail.com>
2022-12-22 12:39:33 +0200

    new

--- pages/cpu_page.py
+++ pages/cpu_page.py
@@ -16,12 +16,28 @@ class CPU_page(Base):
     # Locators
 
     select_cpu_list = '//div[@data-id="10000"]'
+
     select_price_slider_left = '//*[@id="price-slider"]/span[1]'
     select_price_slider_right = '//*[@id="price-slider"]/span[2]'
 
     select_price_input_min = '//input[@id="f-price-l"]'
     select_price_input_max = '//input[@id="f-price-h"]'
     select_price_button_ok = '//*[@id="price-diap"]/div[3]/button'
+
+    select_item_list_manufacture = '//*[@id="cnf-content"]/div/div/div[4]/div[2]/div[1]/div[2]/aside/form/div[2]/ul'
+    select_item_list_socket = '//*[@id="cnf-content"]/div/div/div[4]/div[2]/div[1]/div[2]/aside/form/div[3]/ul'
+    select_item_list_model_range = '//*[@id="cnf-content"]/div/div/div[4]/div[2]/div[1]/div[2]/aside/form/div[4]/ul'
+    select_item_list_intel_generation = '//*[@id="cnf-content"]/div/div/div[4]/div[2]/div[1]/div[2]/aside/form/div[2]/ul'
+    select_item_list_amd_ryzen_series = '//*[@id="cnf-content"]/div/div/div[4]/div[2]/div[1]/div[2]/aside/form/div[6]/ul'
+    select_item_list_total_number_of_cores = '//*[@id="cnf-content"]/div/div/div[4]/div[2]/div[1]/div[2]/aside/form/div[7]/ul'
+    select_item_list_number_of_threads = '//*[@id="cnf-content"]/div/div/div[4]/div[2]/div[1]/div[2]/aside/form/div[8]/ul'
+    select_item_list_max_memory_frequency = '//*[@id="cnf-content"]/div/div/div[4]/div[2]/div[1]/div[2]/aside/form/div[9]/ul'
+    select_item_list_with_integrated_video_core = '//*[@id="cnf-content"]/div/div/div[4]/div[2]/div[1]/div[2]/aside/form/div[10]/ul'
+    select_item_list_free_multiplier = '//*[@id="cnf-content"]/div/div/div[4]/div[2]/div[1]/div[2]/aside/form/div[11]/ul'
+    select_item_list_tech_process = '//*[@id="cnf-content"]/div/div/div[4]/div[2]/div[1]/div[2]/aside/form/div[12]/ul'
+    select_item_list_type_of_packaging = '//*[@id="cnf-content"]/div/div/div[4]/div[2]/div[1]/div[2]/aside/form/div[13]/ul'
+    select_item_list_with_cooler_included = '//*[@id="cnf-content"]/div/div/div[4]/div[2]/div[1]/div[2]/aside/form/div[14]/ul'
+
 
     # Getters
 
@@ -83,4 +99,25 @@ class CPU_page(Base):
         self.click_price_button_ok()
 
 
-
+# item_list_3 = WebDriverWait(self.driver, 30).until(EC.visibility_of_all_elements_located((By.XPATH,
+#                                                                          '//*[@id="cnf-content"]/div/div/div[4]/div[2]/div[1]/div[2]/aside/form/div[4]/ul')))
+#
+#
+#
+#         s = [i.text for i in item_list_3]
+#         my_list = list(map(str, s[0].split('\n')))
+#
+#         for item_3 in my_list:
+#             n = ' '.join(str(item_3).split(' ')[:-1])
+#             print(n)
+#
+#             element = self.driver.find_element(By.XPATH, f'//*[text()="{n} "]')
+#             try:
+#                 self.driver.execute_script("arguments[0].scrollIntoView();", element)
+#             # actions = ActionChains(self.driver)
+#             # actions.move_to_element(element).perform()
+#                 WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, f'//*[text()="{n} "]'))).click()
+#                 time.sleep(0.3)
+#             except Exception:
+#                 continue
+#         print('Prin List 3 --------------------------------')
