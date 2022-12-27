0abb428839cda9ec7a89c7ebccc65bf173e2764b
“Muzyria” <fila090580@gmail.com>
2022-12-27 16:07:03 +0200

    new

--- pages/main_page_flagman.py
+++ pages/main_page_flagman.py
@@ -18,17 +18,33 @@ class Main_page_flagman(Base):
     # Locators
 
     select_link_spinning = '//i[@class="icon icon-menu_item_2"]'
+    select_link_spinning_2 = '/html/body/div[2]/header/div[2]/div/ul/li[2]/div/div/ul/li[1]/a[2]/div'
+    select_link_lang_switch_ru = '//a[text()="Рус"]'
 
     # Getters
 
     def get_link_spinning(self):
         return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.select_link_spinning)))
 
+    def get_link_spinning_2(self):
+        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.select_link_spinning_2)))
+
+    def get_link_lang_switch_ru(self):
+        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.select_link_lang_switch_ru)))
+
     # Actions
 
     def click_link_spinning(self):
         self.get_link_spinning().click()
         print('Click link_spinning')
+
+    def click_link_spinning_2(self):
+        self.get_link_spinning_2().click()
+        print('Click link_spinning_2')
+
+    def click_link_lang_switch_ru(self):
+        self.get_link_lang_switch_ru().click()
+        print('Click lang_switch_ru')
 
     # Methods
 
@@ -37,5 +53,7 @@ class Main_page_flagman(Base):
         self.driver.maximize_window()
         self.get_current_url()
         self.click_link_spinning()
+        self.click_link_spinning_2()
+        self.click_link_lang_switch_ru()
 
 
---
+++ pages/spinning_page.py
@@ -0,0 +1,55 @@
+import time
+
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from base.base_class import Base
+
+
+class Spinning_page(Base):
+
+    def __init__(self, driver):
+        super().__init__(driver)
+        self.driver = driver
+
+    # Locators
+
+    select_subscribe_deny = '//div[@id="subscribe-deny"]'
+    select_button_skip_tip = '//b[@class="dg-btn dg-skip"]'
+    select_button_cpu_list = '//*[@id="cnf-content"]/div/div/div[4]/div[2]/div[1]/div'
+
+    # Getters
+
+    def get_subscribe_deny(self):
+        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.select_subscribe_deny)))
+
+    def get_button_skip_tip(self):
+        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.select_button_skip_tip)))
+
+    def get_button_cpu_list(self):
+        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.select_button_cpu_list)))
+
+    # Actions
+
+    def click_subscribe_deny(self):
+        self.get_subscribe_deny().click()
+        print('Click subscribe deny')
+
+    def click_button_skip_tip(self):
+        self.get_button_skip_tip().click()
+        print('Click button skip tip')
+
+    def click_button_cpu_list(self):
+        self.get_button_cpu_list().click()
+        print('Click button_cpu_list')
+
+    # Methods
+
+    def run_configurator(self):
+        self.get_current_url()
+        self.click_subscribe_deny()
+        self.click_button_skip_tip()
+        self.click_button_cpu_list()
+
+
