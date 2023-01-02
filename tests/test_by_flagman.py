3d17531603b0f0927e1c891657d80697102647da
Muzyria <fila090580@gmail.com>
2023-01-03 00:08:48 +0200

    new

--- tests/test_by_flagman.py
+++ tests/test_by_flagman.py
@@ -21,10 +21,10 @@ def test_by_main_page():
 
     spin = Spinning_page(driver)
     spin.run_producer_list_configurator()
-    # spin.run_price_configurator()
-    # spin.run_length_configurator()
-    # spin.run_minimum_tes_t_configurator()
-    # spin.run_maximum_tes_t_configurator()
+    spin.run_price_configurator()
+    spin.run_length_configurator()
+    spin.run_minimum_tes_t_configurator()
+    spin.run_maximum_tes_t_configurator()
 
     # spin.run_type_of_rod_configurator()
 
